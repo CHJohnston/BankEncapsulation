77b0458981fa9b0b38940960397ada1534e413d1
CHJohnston <cdh.johnston@gmail.com>
2020-07-01 10:46:45 -0500

    Add project files.

---
+++ BankEncapsulationConsoleUI/BankAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulationConsoleUI
+{
+    public class BankAccount
+    {
+        //Create a private field of type double named balance with a value of 0
+        private double _balance = 0;
+
+        //Define Method named Deposit that will accept a double and store the value in the balance field
+        public double Deposit(double amount) 
+        {
+            _balance += amount;
+            return _balance;
+        }
+
+        //Define a Method named GetBalance that will return the amount stored in the balance field
+        public double GetBalance()
+        {          
+            return _balance;
+        }
+    }
+}
---
+++ BankEncapsulationConsoleUI/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BankEncapsulationConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate a new instance of the BankAccount class
+            BankAccount customer = new BankAccount();
+
+            //Accept the user input of the deposit amount 
+            Console.Write("Enter the amount for your deposit :");
+            double depositamount = double.Parse(Console.ReadLine());
+
+            //Pass the deposit amount to the Deposit method for the customer that will add it to their balance
+            customer.Deposit(depositamount);
+                      
+            //Print out the new balance using the Get Balance method for the customer
+            Console.Write($"Your Current Balance is {customer.GetBalance()}");
+        }
+    }
+}
