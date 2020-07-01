4a4e7d3db36ac98e7dc9b4515881a33b983bbdc3
CHJohnston <cdh.johnston@gmail.com>
2020-07-01 13:26:59 -0500

    Added PersonalInformation Class

--- BankEncapsulationConsoleUI/Program.cs
+++ BankEncapsulationConsoleUI/Program.cs
@@ -10,15 +10,24 @@ namespace BankEncapsulationConsoleUI
             BankAccount customer = new BankAccount();
 
             //Accept the user input of the deposit amount 
-            Console.Write("Enter the amount for your deposit : ");
-            double depositamount = double.Parse(Console.ReadLine());
-            Console.WriteLine();
+            Console.Write("Enter your Deposit Amount: ");
+            double depositamount = double.Parse(Console.ReadLine());           
 
             //Pass the deposit amount to the Deposit method for the customer that will add it to their balance
             customer.Deposit(depositamount);
                       
             //Print out the new balance using the Get Balance method for the customer
-            Console.Write($"Your Current Balance is {customer.GetBalance(), 0:c}");
+            Console.WriteLine($"Your Current Balance is {customer.GetBalance(), 0:c}");
+            Console.WriteLine();
+
+            //Instantiate a new instance of the PersonalInformation Class
+            PersonalInformation personalInformation = new PersonalInformation();
+            //Ask the user to input their phone number
+            Console.Write("Enter your phone number: ");
+            personalInformation.NumberFormatted = Console.ReadLine();
+            //Print out the formatted phone number
+            Console.Write($"You entered the following phone number {personalInformation.NumberFormatted}");
+            Console.WriteLine();
         }
     }
 }
